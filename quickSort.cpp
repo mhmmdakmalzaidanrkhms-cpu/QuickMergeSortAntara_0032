274fcb798aca0909fd18d151fd07a6fe0df433b5
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-07-21 14:46:35 +0700

    Implement input function to validate array size with user prompts

--- quickSort.cpp
+++ quickSort.cpp
@@ -6,3 +6,20 @@ int cmp_count = 0; //inisialisasi variabel untuk jumlah perbandingan
 int mov_count = 0; //inisialisasi variabel untuk jumlah pergerakan data
 int n;             //inisialisasi variabel n untuk panjang elemen array
 
+void input()   //membuat prosedur input
+{
+    while (true) //looping untuk meminta input panjang elemen array
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20) //jika jumlah elemen kurang dari atau sama dengan 20
+        {
+            break;
+        }
+        else //jika jumlah elemen lebih dari 20 permintaan input akan diulang
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+
